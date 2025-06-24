15811be60967e0ec071244dcd4f5866f4a6c9f32
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2025-06-24 05:07:15 -0700

    mainer changes in manu router and server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let app = express();
 const bodyParser = require("body-parser");
 const dbConnection = require("./db");
 
+// Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
 
@@ -15,55 +16,9 @@ app.use('/', personRoute)
 app.use('/', manuRoute)
 
 
-
- 
+// Starting the server and connecting to the database
     dbConnection().then(() => {
       app.listen(3000, () => {
         console.log("server is runing on port 3000");
       });
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//creat modul schima
-//post
-//db conection
-//then start server
-//send json data
-
-//tu jalDhara is badal ki
-//tu premika is pagal ki
-//
-
-//tu bund hai mere sagar ki
-//na jane meri geherai
-//na muzme ese tu gul jana
-//tu mithi bund mai hoo khara
-
-//jis nadi se aai thi beheke
-//tuze lotadu usme fir se
-//na abse muza me tu behena na
-//na mil jana fir dobara
